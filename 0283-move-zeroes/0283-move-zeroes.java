b23f80b18ade2daeb416946f3b0875cc9a54aeb2
Saket Bhasin <bhasinsaket1@gmail.com>
2026-08-30 19:29:10 +0530

    Time: 3 ms (17.96%), Space: 47.9 MB (38.4%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int j=0;
+        for(j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+        }
+        
+    }
+}
